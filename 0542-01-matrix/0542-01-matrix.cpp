763fffcd635b785a3bebd681fb2274e796f6a0ce
Aditya <141512093+aditya17raj@users.noreply.github.com>
2026-08-04 00:23:53 +0530

    Time: 15 ms (72.22%), Space: 38 MB (44.69%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -4,18 +4,16 @@ public:
         int n=mat.size();
         int m=mat[0].size();
 
-        vector<vector<int>> dis(n, vector<int>(m,0));
-        vector<vector<bool>> visited(n, vector<bool> (m,false));
+        vector<vector<int>> dis(n, vector<int> (m,0));
+        vector<vector<bool>> visited(n, vector<bool>(m, false));
 
-        queue<pair<pair<int,int>, int>> q;
+        queue<pair<pair<int,int> , int>> q;
 
         for(int i=0; i<n; i++){
             for(int j=0; j<m; j++){
                 if(mat[i][j] == 0){
-                    q.push({ {i,j}, 0 });
-                    visited[i][j]=true;
-                }else{
-                    visited[i][j]=false;
+                    q.push({{i,j}, 0});
+                    visited[i][j] = true;
                 }
             }
         }
@@ -24,26 +22,26 @@ public:
         vector<int> delcol = {-1,0,1,0};
 
         while(!q.empty()){
-            auto front=q.front();
+            int row = q.front().first.first;
+            int col = q.front().first.second;
+            int s = q.front().second;
+
             q.pop();
 
-            int r=front.first.first;
-            int c=front.first.second;
-            int s=front.second;
-
-            dis[r][c] = s;
+            dis[row][col] = s;
 
             for(int i=0; i<4; i++){
-                int nrow=r+delrow[i];
-                int ncol=c+delcol[i];
+                int nrow = row+delrow[i];
+                int ncol = col+delcol[i];
 
-                if( (nrow>=0 && nrow<n) && (ncol>=0 && ncol<m) && !visited[nrow][ncol]){
-                    q.push({{nrow,ncol} , s+1 });
+                if( (nrow>=0 && nrow<n) && (ncol>=0 && ncol<m) && !visited[nrow][ncol] ){
+                    q.push({ {nrow,ncol}, s+1});
                     visited[nrow][ncol] = true;
                 }
             }
         }
 
         return dis;
+
     }
 };
