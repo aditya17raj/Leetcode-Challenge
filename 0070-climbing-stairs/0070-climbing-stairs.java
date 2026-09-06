85f9b695b5a7864da78d507b2f7403eeaad44068
Aditya <141512093+aditya17raj@users.noreply.github.com>
2026-09-06 13:58:39 +0530

    Time: 0 ms (100%), Space: 42.3 MB (17.38%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int climbStairs(int n) {
+        if(n==1 || n==2)
+            return n;
+
+        int x=1, y=2, ans=0;
+
+        for(int i=3; i<=n; i++){
+            ans = x+y;
+
+            x=y;
+            y=ans;
+        }
+
+        return ans;
+    }
+}
